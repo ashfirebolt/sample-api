76038a17f3fca0437a6b32fe6ea16a4ee56b5916
Avinash Mavireddi <ashfirebolt@gmail.com>
2018-07-07 23:17:05 -0500

    Initial commit

---
+++ src/main/java/io/amavireddi/springbootstarter/SampleApiApp.java
@@ -0,0 +1,14 @@
+package io.amavireddi.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApiApp.class, args);
+
+	}
+
+}
